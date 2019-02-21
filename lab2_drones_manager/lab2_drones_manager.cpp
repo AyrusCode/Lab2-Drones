2b06cfdac479ec379adc03290d624b9bc35a3d5e
Surya Pandiaraju <surya.pandiaraju@hotmail.com>
2019-02-21 16:23:14 -0500

    Update lab2_drones_manager.cpp

--- lab2_drones_manager/lab2_drones_manager.cpp
+++ lab2_drones_manager/lab2_drones_manager.cpp
@@ -1,90 +1,95 @@
-#include "lab2_drones_manager.h"
+#include "lab2_drones_manager.hpp"
+#include <iostream>
 
 // TODO: Implement all of the listed functions below
 
 DronesManager::DronesManager() {
+	size = 0;
 	first = NULL;
 	last = NULL;
-	size = 0;
+
 }
 
 DronesManager::~DronesManager() {
-	DroneRecord* curr=first;
-	while (curr) {
-	DroneRecord* prev = curr;
-		curr = curr->next;
-		delete prev;
+	DroneRecord* currentNode = first;
+	while (currentNode) {
+		DroneRecord* previousNode = currentNode;
+		currentNode = currentNode->next;
+		delete previousNode;
 	}
 }
 
 bool operator==(const DronesManager::DroneRecord& lhs, const DronesManager::DroneRecord& rhs) {
-	bool is_equal=true;
-	is_equal = is_equal && lhs.droneID == rhs.droneID;
-	is_equal = is_equal && lhs.range == rhs.range;
-	is_equal = is_equal && lhs.yearBought== rhs.yearBought;
-	is_equal = is_equal && lhs.droneType == rhs.droneType;
-	is_equal = is_equal && lhs.manufacturer == rhs.manufacturer;
-	is_equal = is_equal && lhs.description == rhs.description;
-	is_equal = is_equal && lhs.batteryType== rhs.batteryType;
-	return is_equal;
+	bool isSame = true;
+	isSame = isSame && lhs.droneID == rhs.droneID;
+	isSame = isSame && lhs.range == rhs.range;
+	isSame = isSame && lhs.yearBought == rhs.yearBought;
+	isSame = isSame && lhs.droneType == rhs.droneType;
+	isSame = isSame && lhs.manufacturer == rhs.manufacturer;
+	isSame = isSame && lhs.description == rhs.description;
+	isSame = isSame && lhs.batteryType == rhs.batteryType;
+
+	return isSame;
 }
 
 unsigned int DronesManager::get_size() const {
-	DroneRecord*curr = first;
-	int counter = 0;
-	while (curr) {
-		curr = curr->next;
-		counter++;
+	DroneRecord* currentNode = first;
+	int numNodes = 0;
+	while (currentNode) {
+		numNodes++;
+		currentNode->next;
 	}
-	return counter;
+	return numNodes;
 }
 
 bool DronesManager::empty() const {
-	if (size == 0)
+	if (size == 0) {
 		return true;
+	}
+
 	return false;
 }
 
 DronesManager::DroneRecord DronesManager::select(unsigned int index) const {
-	DroneRecord*curr = first;
-
-	if (size == 0);
-		return *curr;
-
-	if (size < index)
+	DroneRecord* current = first;
+	if (size <= index) {
 		return *last;
-
-	for (int counter = 0; counter < index; counter++) {
-		curr = curr->next;
 	}
 
-	DroneRecord curr_record = *curr;
-	return curr_record;
+	for (int i = 0; i < index; i++) {
+		current = current->next;
+	}
+
+	return *current;
 }
 
 unsigned int DronesManager::search(DroneRecord value) const {
-	DroneRecord* curr = first;
-	int counter = 0;
-	if (size == 0);
-		return 0;
+	DroneRecord* current = first;
 
-	while (curr) {
-		curr = curr->next;
-		DroneRecord curr_Record = *curr;
-		if (curr_Record == value);
-			return counter;
-		counter++;
+	for (int i = 0; i < size; i++) {
+		if (*current == value) {
+			return i;
+		}
+		current = current->next;
 	}
-	return counter;
+
+	return size;
+
 }
 
 void DronesManager::print() const {
+	DroneRecord* current = first;
+//	<< range << yearBought << droneType << manufacturer << description << batteryType;
+	for (int i = 0; i < size; i++) {
+		DroneRecord toPrint = *current;
+		toPrint.printRecord();
+		*current = *current->next;
+	}
 
 }
-
 bool DronesManager::insert(DroneRecord value, unsigned int index) {
 	DroneRecord* curr = first;
-	DroneRecord* previous;
+	DroneRecord* previous = curr;
 	DroneRecord* val_pointer = &value;
 
 	if (size < index)
@@ -94,43 +99,67 @@ bool DronesManager::insert(DroneRecord value, unsigned int index) {
 		previous = curr;
 		curr = curr->next;
 	}
+	if (index != 0) {
+		previous->next = val_pointer;
+		val_pointer->prev = previous;
+		val_pointer->next = curr;
+		curr->prev = val_pointer;
+	}
+	
+	else {
+		insert_front(value);
 
-	previous->next = val_pointer;
-	val_pointer->prev = previous;
-	val_pointer->next = curr;
-	curr->prev = val_pointer;
-
+	}
 	return true;
+
 }
 
 bool DronesManager::insert_front(DroneRecord value) {
-	DroneRecord* curr = first;
-	*first = value;
-	first->next = curr;
-	curr->prev = first;
+	DroneRecord* oldFirst = first;
+	if (size == 0) {
+		first = &value;
+		last = first;
+		size++;
+		return true;
+	}
+	oldFirst->next = first->next;
+	first = &value;
+	first->next = oldFirst;
+	oldFirst->prev = first;
 	first->prev = NULL;
+	size++;
 	return true;
 }
 
 bool DronesManager::insert_back(DroneRecord value) {
-	DroneRecord* val_pointer = &value;
-	last->next = val_pointer;
-	val_pointer->prev = last;
-	return true;	
+	DroneRecord* newLast = &value;
+
+	if (size == 0) {
+		last = &value;
+		first = last;
+		return true;
+	}
+
+	last->next = newLast;
+	newLast ->prev = last;
+	size++;
+	return true;
 }
 
 bool DronesManager::remove(unsigned int index) {
 	DroneRecord* curr = first;
 	DroneRecord* previous;
+
 	for (int i = 0;i < index; i++) {
 		previous = curr;
 		curr = curr->next;
 	}
 
-	DroneRecord* curr_next=curr->next;
+	DroneRecord* curr_next = curr->next;
 	previous = curr_next;
 	curr_next->prev = previous;
 	delete curr;
+	size--;
 	return true;
 }
 
@@ -140,6 +169,7 @@ bool DronesManager::remove_front() {
 	first->prev = NULL;
 	old_first->next = NULL;
 	delete old_first;
+	size--;
 	return true;
 }
 
@@ -148,46 +178,45 @@ bool DronesManager::remove_back() {
 	last = last->prev;
 	last->next = NULL;
 	delete old_last;
+	size--;
 	return false;
 }
 
 bool DronesManager::replace(unsigned int index, DroneRecord value) {
-	if (index < size)
-		return false;
-
+	if (index < size) return false;
 	DroneRecord* curr = first;
-	DroneRecord* previous;
+	DroneRecord* previous = curr;
 	DroneRecord* replacement = &value;
+
 	for (int i = 0; i < index; i++) {
 		previous = curr;
 		curr = curr->next;
 	}
+
 	previous->next = replacement;
 	replacement->next = curr->next;
 	curr->next->prev = replacement;
 	replacement->prev = previous;
 	delete curr;
+
 	return true;
+
 }
 
 bool DronesManager::reverse_list() {
-	DroneRecord *curr = last;
-	DroneRecord *previous = last->prev;
-	DroneRecord *temp_one = last;
-	DroneRecord* temp_two = last->prev;
-	DroneRecord* last_temp = last;
+	if (first == NULL) return false;
 
-	curr->prev = NULL;
-	while (previous) {
-		curr->next = previous;
-		previous->prev=curr;
-		curr = temp_one->prev;
-		previous = temp_two->prev;
+	DroneRecord* previous = NULL;
+	DroneRecord* current = first; 
+	DroneRecord* next = NULL;
+
+	while (current != NULL) {
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
 	}
-	curr->next = NULL;
-	last = first;
-	first = last_temp;
-	  
-	return false;
-}
 
+	first = previous;
+	return true;
+}
